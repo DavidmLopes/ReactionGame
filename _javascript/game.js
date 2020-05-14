ad16f12d8282080e9de94b23ad09dcd986614947
Masterr15 <david.mlopes15@gmail.com>
2020-05-14 20:38:50 +0100

    Action when circle clicked - makes new circle

--- _javascript/game.js
+++ _javascript/game.js
@@ -1,9 +1,20 @@
 function start() {
     document.getElementById("startButton").style.display = "none";
     var circle = document.getElementById("circle");
+    circle.style.display = "inline";
+    document.getElementById("game").style.paddingLeft = circle.offsetWidth + "px";
+    document.getElementById("game").style.paddingTop = circle.offsetHeight + "px";
+    makeCircle();
+}
+
+function makeCircle() {
+    var circle = document.getElementById("circle");
     var top = Math.random() * 100;
     var left = Math.random() * 100;
-    circle.style.display = "inline";
     circle.style.top = "calc(" + top + "% - " + circle.offsetHeight + "px)";
     circle.style.left = "calc(" + left + "% - " + circle.offsetWidth + "px)";
 }
+
+document.getElementById("circle").onclick = function() {
+    makeCircle();
+};
